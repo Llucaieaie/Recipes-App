162b7940c0cbe76f997d67f30f6078b4db8ce551
Lluc <lluc.estruch@gmail.com>
2023-11-29 10:29:58 +0100

    data using API

---
+++ recipesapp/lib/models/recipeAPI.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:recipesapp/models/recipe.dart';
+import 'package:http/http.dart' as http;
+
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list',
+        {"limit": "18", "start": "0", "tag": "list.recipe.popular"});
+
+    final response = await http.get(uri, headers: {
+      "x-rapidapi-key": "119ce3ec6fmsh14cb9fa090032e3p1643dcjsnd91aeef1a5ef",
+      "x-rapidapi-host": "yummly2.p.rapidapi.com",
+      "useQueryString": "true"
+    });
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['feed']) {
+      _temp.add(i['content']['details']);
+    }
+
+    return Recipe.recipesFromSnapshot(_temp);
+  }
+}
--- recipesapp/lib/screens/home.dart
+++ recipesapp/lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:recipesapp/models/recipe.dart';
+import 'package:recipesapp/models/recipeAPI.dart';
 import 'package:recipesapp/screens/widgets/recipe_card.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,6 +11,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late List<Recipe> _recipes;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getRecipes();
+  }
+
+  Future<void> getRecipes() async {
+    _recipes = await RecipeApi.getRecipe();
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,11 +43,16 @@ class _HomeScreenState extends State<HomeScreen> {
             ],
           ),
         ),
-        body: RecipeCard(
-          title: 'Recipe title',
-          rating: '5.0',
-          cookTime: '30 min',
-          thumbnailUrl: 'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360',
-        ));
+        body: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: _recipes.length,
+                itemBuilder: (context, index) {
+                  return RecipeCard(
+                      title: _recipes[index].name,
+                      cookTime: _recipes[index].time,
+                      rating: _recipes[index].rating.toString(),
+                      thumbnailUrl: _recipes[index].images);
+                }));
   }
 }
